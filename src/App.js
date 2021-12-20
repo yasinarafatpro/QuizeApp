bc657f45d70335048d9fc97eb2856e68683beee6
yasinarafatpro <yasinarafat1194@gmail.com>
2021-12-20 23:15:16 +0600

    score view

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const questions = [
+		{
+			questionText: 'What is your Mother,s Language ?',
+			answerOptions: [
+				{ answerText: 'English', isCorrect: false },
+				{ answerText: 'Bangla', isCorrect: false },
+				{ answerText: 'Arabic', isCorrect: true },
+				{ answerText: 'Hinde', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Who is CEO of Tesla?',
+			answerOptions: [
+				{ answerText: 'Jeff Bezos', isCorrect: false },
+				{ answerText: 'Elon Musk', isCorrect: true },
+				{ answerText: 'Bill Gates', isCorrect: false },
+				{ answerText: 'Tony Stark', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'The iPhone was created by which company?',
+			answerOptions: [
+				{ answerText: 'Apple', isCorrect: true },
+				{ answerText: 'Intel', isCorrect: false },
+				{ answerText: 'Amazon', isCorrect: false },
+				{ answerText: 'Microsoft', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'How many Harry Potter books are there?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+	];
+	const [currentQuestion,setCurrentQuestion]=useState(0)
+	const [showScore,setShowScore]=useState(false)
+	const [score,setScore]=useState(0)
+
+	const handleAnswer=(isCorrect)=>{
+		if(isCorrect===true){
+			setScore(score + 1)
+		}
+		const nexQuestion=currentQuestion+1
+		if(nexQuestion < questions.length){
+			setCurrentQuestion(nexQuestion)
+		}else{
+			setShowScore(true)
+		}
+		
+	}
+	return (
+		<div className='app'>
+			{showScore ? (
+				<div className='score-section'>
+					You scored {score} out of {questions.length}
+				</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map((answerOptions)=>
+						<button onClick={()=>handleAnswer(answerOptions.isCorrect)}>{answerOptions.answerText}</button>)}
+					</div>
+				</>
+			)}
+		</div>
+	);
 }
-
-export default App;
